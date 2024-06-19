1566fb2a4c2e33a1be33869854cef3140538e090
Guo Zihao <778992212@qq.com>
2024-06-19 11:38:44 +0800

    feat:dto

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Patch, Query } from '@nestjs/common';
 import { count } from 'console';
 
 @Controller('question')
 export class QuestionController {
-  @Get() findAll() {
+  @Get() findAll(
+    @Query('keyword') keyword: string,
+    @Query('page') page: number,
+    @Query('size') size: number,
+  ) {
     return { list: ['a', 'b', 'c'], count: 10 };
+  }
+  @Get('id') findOne(@Param('id') id: string) {
+    return { id, title: 'aaa', desc: 'bbb' };
+  }
+  @Patch('id') updateOne(@Param('id') id: string) {
+    return { id, title: 'aaa', desc: 'bbb' };
   }
 }
